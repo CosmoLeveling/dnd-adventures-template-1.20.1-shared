bc8e828739c1ca64387d2f6944a4bff8a8e2c5bc
cosmo <curtisjibo@gmail.com>
2024-04-16 18:31:51 -0500

    here is the repo

--- src/main/java/com/cosmo/dndadventures/DndAdventures.java
+++ src/main/java/com/cosmo/dndadventures/DndAdventures.java
@@ -3,8 +3,11 @@ package com.cosmo.dndadventures;
 import com.cosmo.dndadventures.networking.ModMessages;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static com.cosmo.dndadventures.ModRegisteryKeys.RACES_KEY;
 
 public class DndAdventures implements ModInitializer {
 	public static String MOD_ID = "dnd-adventures";
@@ -18,5 +21,20 @@ public class DndAdventures implements ModInitializer {
 		DndAdventures.LOGGER.info(DndAdventures.MOD_ID + " resources registered!");
 		ModMessages.registerC2SPackets();
 		ModMessages.registerS2CPackets();
+
+		ServerLifecycleEvents.SERVER_STARTED.register(server->{
+			server.getRegistryManager()
+					.getOptional(RACES_KEY)
+					.ifPresent(registry->{
+						registry
+								.streamEntries()
+								.forEach(entry->{
+									System.out.println(entry.registryKey()
+											.getValue()
+											.toString()
+									);
+								});
+					});
+		});
 	}
 }
